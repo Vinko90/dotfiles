5bd9f3c07661e3f6c7527bad8fff6b526c08740e
WarOfDevil <vnktux@protonmail.com>
2019-01-21 09:45:39 +0100

    Add support for dual screen

---
+++ i3/scripts/dual.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS-1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output DP-1 --mode 2560x1440 --pos 1600x0 --rotate normal --output HDMI-1 --off --output VGA-1 --off
